2cc246a9c76de8130b03c4714f1c629da5076404
ToxicoFromMexico <jamie.obst.deutschland@gmail.com>
2025-07-25 20:10:16 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,62 @@
+config = {
+
+	language = "de", -- de=german / en=english / cs=custom
+
+	timeToRob = 5, -- In minutes (5 minutes standard)
+	robberyCooldown = 15, -- In minutes (15 minutes standard)
+
+    useRandomReward = true, -- true = random, false = fix
+    fixedReward = 10000, -- fixed reward if = false
+    minReward = 2000, -- minimum random reward
+    maxReward = 10000, -- maximum random reward
+
+	shopcoords = { -- Coordinates of the robbable stores
+		{name = "24/7 Senora Fwy // Mount Chilliad Robbery",
+		x = 1734.54, y = 6420.27, z = 35.04 - 1},
+
+		{name = "24/7 on Senora Fwy // Grand Senora Desert Robbery",
+		x = 2673.21, y = 3286.36, z = 55.24 - 1},
+
+		{name = "Harmony 24/7 Robbery",
+		x = 546.35, y = 2663.35, z = 42.16 - 1},
+
+		{name = "Sandy 24/7 Robbery",
+		x = 1960.71, y = 3748.53, z = 32.36 - 1},
+
+		{name = "Chumash 24/7 Robbery",
+		x = -3249.52, y = 1004.41, z = 12.83 - 1},
+
+		{name = "Banham Canyon 24/7 Robbery",
+		x = -3047.39, y = 585.85, z = 7.91 - 1},
+
+		{name = "Palomino Fwy 24/7 Robbery",
+		x = 2549.84, y = 384.93, z = 108.64 - 1},
+
+		{name = "Vinewood 24/7 Robbery",
+		x = 377.99, y = 332.96, z = 103.57 - 1},
+
+		{name = "Strawberry 24/7 Robbery",
+		x = 28.71, y = -1339.51, z = 29.5 - 1},
+
+		{name = "Grapeseed LTD Robbery",
+		x = 1707.27, y = 4919.77, z = 42.09 - 1},
+
+		{name = "Richman Glen LTD Robbery",
+		x = -1828.57, y = 799.13, z = 138.20 - 1},
+
+		{name = "Little Seoul LTD Robbery",
+		x = -709.11, y = -904.12, z = 19.22 - 1},
+
+		{name = "Davis LTD Robbery",
+		x = -42.99, y = -1748.79, z = 29.42 - 1},
+
+		{name = "Mirror Park LTD Robbery",
+		x = 1160.38, y = -313.88, z = 69.21 - 1},
+
+		{name = "Vespucci Canals Liquor Store Robbery",
+		x = -1220.25, y = -915.79, z = 11.33 - 1},
+
+		{name = "Harmony Liquor Store Robbery",
+		x = 1168.83, y = 2718.03, z = 37.16 - 1},
+	}
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Toxic'
+description 'simple robbery script for ND Core'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua' --configuration
+}
+
+client_scripts {
+    'script/client.lua' --code
+}
+
+server_scripts {
+    'script/server.lua' --code
+}
